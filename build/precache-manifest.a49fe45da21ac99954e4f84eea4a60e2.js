a4b3b5b6ee53f9eec2cacca8edc8d1a151ded66d
Pardeep Bhatt <pardeepbhatt5254@gmail.com>
2020-03-15 22:40:25 +0530

    build added

---
+++ build/precache-manifest.a49fe45da21ac99954e4f84eea4a60e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3487761c43e4361f816f7f96546065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d3b5d70d92928bc94f0",
+    "url": "/static/css/main.c3854ecf.chunk.css"
+  },
+  {
+    "revision": "d6384579b5f747a2db41",
+    "url": "/static/js/2.85272dda.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.85272dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3b5d70d92928bc94f0",
+    "url": "/static/js/main.791aa41b.chunk.js"
+  },
+  {
+    "revision": "317ffd99d1ecd3c263bb",
+    "url": "/static/js/runtime-main.3791ac7a.js"
+  }
+]);
